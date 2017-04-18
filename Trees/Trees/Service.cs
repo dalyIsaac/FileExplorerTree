1529af8e8b41790058deb259770339896ebe5d0e
Isaac Daly <isaac.daly.dev@outlook.com>
2017-04-18 19:08:44 +1200

    Working without columns

---
+++ Trees/Trees/Service.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trees
+{
+    public class Service
+    {
+        private readonly Collection<Items> itemList;
+
+        public IEnumerable<Items> ItemList
+        {
+            get { return itemList; }
+        }
+
+        public Service(string path)
+        {
+            itemList = new Collection<Items> { new Items(path, true) };
+        }
+    }
+}
